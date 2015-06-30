f06a8bc04f030dc8ba364963cb0c78032ce971a3
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-06-30 17:09:58 -0400

    Add a failing test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
 //! [Discrete wavelet transform][1].
 //!
 //! [1]: https://en.wikipedia.org/wiki/Discrete_wavelet_transform
+
+/// Perform the forward transformation.
+pub fn forward(_: &mut [f64]) {
+}
